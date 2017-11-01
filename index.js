62e4d67ce4e40c490888c1353a3f67483d0e1e9d
Borek Bernard <borekb@gmail.com>
2017-11-01 10:11:01 +0100

    Improved regex to handle a couple more non-matches

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 'use strict';
-module.exports = () => (/(?:[\w-.]+\/[\w-.]+|\B)#[1-9]\d*/g);
+// https://regex101.com/r/SQrOlx/12
+module.exports = () => (/(?:\w[\w-.]+\/\w[\w-.]+|\B)#[1-9]\d*\b/g);
--- test.js
+++ test.js
@@ -16,7 +16,9 @@ const nonMatches = [
 	'sindresorhus/dofle',
 	'sindresorhus/dofle#',
 	'sindresorhus/dofle#0',
-	'dofle#33'
+	'dofle#33',
+	'#123hashtag',
+	'non/-repo#123'
 ];
 
 test(t => {
@@ -33,7 +35,7 @@ test(t => {
 		t.is(m().exec(`foo ${x} bar`), null);
 	}
 
-	// https://regex101.com/r/SQrOlx/6
+	// https://regex101.com/r/SQrOlx/12
 	t.deepEqual(
 		`#123
 
@@ -41,17 +43,29 @@ Should match:
 
 - Plain issue: #666
 - From another repository: another/repo#123
-- In brackets: (#123), [#123]
-- Wild formatting: ano-ther.999/re_po#123
+- Crazy formatting: ano-ther.999/re_po#123
+- In brackets: (#123), [#123], <another/repo#123>
 
 Should NOT match:
 
 - #0
 - another/repo#0
 - nonrepo#123
+- non/-repo#123
 - user_repo#123
+- this/is/not/repo#123
+- #123hashtag
 
 #123`.match(m()),
-		['#123', '#666', 'another/repo#123', '#123', '#123', 'ano-ther.999/re_po#123', '#123']
+		[
+			'#123',
+			'#666',
+			'another/repo#123',
+			'ano-ther.999/re_po#123',
+			'#123',
+			'#123',
+			'another/repo#123',
+			'not/repo#123', // Current limitation; this match should not be here
+			'#123']
 	);
 });
