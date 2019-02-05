580a5fcc6bed77cfcfc4ba028e181ef7ea854cb8
Erik Ochoa <elyager@gmail.com>
2019-02-04 18:43:21 -0700

    Require Node.js 10 and use lookbehinds to improve the regex (#5)
    Fixes #3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
 
-// https://regex101.com/r/SQrOlx/12
-module.exports = () => /(?:\w[\w-.]+\/\w[\w-.]+|\B)#[1-9]\d*\b/g;
+// https://regex101.com/r/SQrOlx/14
+module.exports = () => /(?:(?<![/\w-.])\w[\w-.]+\/\w[\w-.]+|\B)#[1-9]\d*\b/g;
