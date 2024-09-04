781ae0c74441ca681ae518e7d0a27911778e0243
Federico Brigante <me@fregante.com>
2024-09-05 00:14:15 +0700

    Extract source

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const {source: repository} = /(?<repository>[\w.-]{1,100})/;
 const {source: reservedRepository} = /(?<!\/\.{1,2})/;
 const {source: issueNumber} = /(?<issueNumber>[1-9]\d{0,9})/;
 const {source: initialDelimiter} = /(?<!\w)/;
+const fullRegex = `${initialDelimiter}(?:${organization}\\/${repository})?${reservedRepository}#${issueNumber}\\b`;
 
 export default function issueRegex() {
-	return new RegExp(
-		`${initialDelimiter}(?:${organization}\\/${repository})?${reservedRepository}#${issueNumber}\\b`,
-		'gi',
-	);
+	return new RegExp(fullRegex, 'gi');
 }
