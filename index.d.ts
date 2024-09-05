10fc0f2be2c41de84344fd635b3725a1612849da
Federico <me@fregante.com>
2024-09-05 15:23:58 +0700

    Types

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,17 @@
 /**
 Regular expression for matching issue references.
 
+@param prefix - Additional prefix for references like `GH-123`. Adding a prefix will still match the #-based references.
+
 @example
 ```
 import issueRegex from 'issue-regex';
 
 'Fixes #143 and avajs/ava#1023'.match(issueRegex());
 //=> ['#143', 'avajs/ava#1023']
+
+'GH-143 is the same as #143'.match(issueRegex('GH-'));
+//=> ['GH-143', '#143']
 ```
 */
-export default function issueRegex(): RegExp;
+export default function issueRegex(prefix?: string): RegExp;
--- index.test-d.ts
+++ index.test-d.ts
@@ -2,3 +2,4 @@ import {expectType} from 'tsd';
 import issueRegex from './index.js';
 
 expectType<RegExp>(issueRegex());
+expectType<RegExp>(issueRegex('GH-'));
